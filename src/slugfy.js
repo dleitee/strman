46b2ddb7a78fcd6b68d367c6f201c8233002e048
Daniel Leite de Oliveira <dleitee@gmail.com>
2016-04-12 09:26:08 -0300

    Update slugfy.js

--- src/slugfy.js
+++ src/slugfy.js
@@ -2,10 +2,12 @@ import {toLowerCase, trim, removeSpaces, replace, removeNonChars} from './lib/st
 import {format} from './lib/format';
 
 /*
+ * Converts a string to a slug.
+ * Example: slugify("A Javascript string manipulation library") => "a-javascript-string-manipulation-library"
  * @param string - value for slugfy
  * @return String - returns a slugfy string.
  */
-export function slugfy(string) {
+export function slugify(string) {
 
     return format(string,
             [toLowerCase,
