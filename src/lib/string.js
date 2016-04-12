5198410b8fc6f5f200d363b38102f735ecdc1bcc
Daniel Leite de Oliveira <dleitee@gmail.com>
2016-04-12 10:00:54 -0300

    Update string.js

--- src/lib/string.js
+++ src/lib/string.js
@@ -1,18 +1,52 @@
 import {ascii} from "./ascii";
 
-export const _isString = value =>
+/*
+ * Checks whether a string
+ * @param value - value to check 
+ * @return Boolean - true or false
+ */
+export const isString = value =>
     Object.prototype.toString.call(value) === '[object String]';
 
+/*
+ * Transform to lowercase
+ * @params value - String to lowercase
+ * @return String
+ */
 export const toLowerCase = value => value.toLowerCase();
 
+/*
+ * Remove all spaces on left and right
+ * @params value - String to trim
+ * @return String without boarders spaces
+ */
 export const trim = value => value.trim();
 
-export const removeSpaces = (replace = "") => (value) => value.replace(/\s+/g, replace)
+/*
+ * Remove all spaces and replace for value
+ * @paramsClojure replace - Value to replace
+ * @param value - The string being searched and replaced on.
+ * @return String without spaces
+ */
+export const removeSpaces = (replace = "") => (value) => _replace(value, "\s+", replace);
 
+/*
+ * Replace [search] value to [newvalue]
+ * @paramsClojure search - String to search
+ * @paramsClojure newvalue - String to replace
+ * @params value - The string being searched and replaced on.
+ * @return String replaced
+ */
 export const replace =
     (search = "", newvalue = "") =>
         (value) => _replace(value, search, newvalue);
 
+/*
+ * Remove all non valid characters 
+ * Example: change á => a or ẽ => e
+ * @params value - The string being searched and replaced on.
+ * @return String without non valid characters.
+ */
 export const removeNonChars = (value) => {
     let result = value;
     for(let key in ascii){
@@ -23,6 +57,11 @@ export const removeNonChars = (value) => {
     return result;
 }
 
+/*
+ * Polyfill to replace function
+ * @params value - The string being searched and replaced on.
+ * @return This function returns a string with the replaced values.
+ */
 let _replace = (value, search, newvalue) => value.replace(new RegExp(search, "g"), newvalue);
 
 
