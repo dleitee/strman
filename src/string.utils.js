beaf323924b62b2c584a2fb2c45939e7097822bc
Daniel Leite de Oliveira <dleitee@gmail.com>
2016-04-18 10:54:09 -0300

    Update string.utils.js

--- src/string.utils.js
+++ src/string.utils.js
@@ -201,17 +201,6 @@ const removeNonWords = (value, replaced = '') => replace(value, '[^\\w]+', repla
 export {removeNonWords};
 
 /*
- * Alias for contains
- * @param value
- * @param needle
- * @param caseSensitive - default true
- * @return boolean
- */
-const includes = (value, needle, caseSensitive = true) => contains(value, needle, caseSensitive);
-
-export {includes};
-
-/*
  * Verifies that the needle is contained in value
  * @param value
  * @param needle
