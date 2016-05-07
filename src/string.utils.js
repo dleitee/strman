0e422a2a9f27f41613391fadf6960db07418772b
Daniel Leite de Oliveira <dleitee@gmail.com>
2016-05-07 10:33:52 -0300

    Fix method surround.

--- src/string.utils.js
+++ src/string.utils.js
@@ -785,7 +785,8 @@ export {shuffle};
  * @param substr
  * @return string
  */
-const surround = (value, _substr = '', _substrRight = null) => append(_substr, value, _substrRight === null ? _substr : _substrRight);
+const surround = (value, _substr = '', _substrRight = null) =>
+    append(_substr, value, _substrRight === null ? _substr : _substrRight);
 
 export {surround};
 
