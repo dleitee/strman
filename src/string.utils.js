7b1593eab262774bf3054f0526f1bc0fad3a46d1
Daniel Leite de Oliveira <dleitee@gmail.com>
2016-05-07 09:24:32 -0300

    Add docs to removeSpaces.

--- src/string.utils.js
+++ src/string.utils.js
@@ -58,10 +58,14 @@ const rightTrim = (value, char = ' ') => replace(value, `${char}+$`, '');
  export {rightTrim};
 
 /**
- * Remove all spaces and replace for value
- * @param replace - Value to replace
- * @param value - The string being searched and replaced on.
- * @return String without spaces
+ * Remove all spaces and replace for value.
+ * @playground
+ * var removeSpaces = require('strman').removeSpaces;
+ * let title = "  s t r  m  a n     ";
+ * let result = removeSpaces(title);
+ * @param {String} value - The string being searched and replaced on.
+ * @param {String} replaced - Value to replace.
+ * @return {String} - String without spaces.
  */
 const removeSpaces = (value, replaced = '') => replace(value, '\\s+', replaced);
 
