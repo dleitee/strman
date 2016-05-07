34612d894ac5c11b4ca5b7cb6e34233d00a46276
Daniel Leite de Oliveira <dleitee@gmail.com>
2016-05-07 09:43:26 -0300

    Add docs to startsWith.

--- src/string.utils.js
+++ src/string.utils.js
@@ -227,7 +227,7 @@ const collapseWhitespace = (value) => trim(replace(value, '\\s\\s+',' '));
 
 export {collapseWhitespace};
 
- /**
+/**
  * Remove all non word characters.
  * var removeNonWords = require('strman').removeNonWords;
  * let title = "__strman../";
@@ -377,11 +377,15 @@ const endsWith = (value, search, position = null, caseSensitive = true) => {
 export {endsWith};
 
 /**
- * Test if [value] starts with [search]
- * @param value
- * @param search
- * @param position = null
- * @return boolean
+ * Test if 'value' starts with 'search'
+ * var startsWith = require('strman').startsWith;
+ * let title = "strman";
+ * let result = startsWith(title, 'str');
+ * @param {String} value - The string being searched.
+ * @param {String} search - Value to search.
+ * @param {Number} position - offset to search.
+ * @param {Boolean = true} caseSensitive - if you use caseSensitive to test.
+ * @return {Boolean} - If 'value' startsWith 'search' return true, else false.
  */
 const startsWith = (value, search, position = 0, caseSensitive = true) => {
 
