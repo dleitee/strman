5b85283feb7b1fe7c18ba5605616ea57b07bfd4d
Daniel Leite de Oliveira <dleitee@gmail.com>
2016-05-07 09:56:20 -0300

    Add docs to lastIndexOf.

--- src/string.utils.js
+++ src/string.utils.js
@@ -484,11 +484,14 @@ export {indexOf};
  * The lastIndexOf() method returns the index within the calling String object of the last
  * occurrence of the specified value, searching backwards from fromIndex. Returns -1 if the
  * value is not found.
- *
- * @param value
- * @param needle
- * @param offset
- * @return integer
+ * var lastIndexOf = require('strman').lastIndexOf;
+ * let title = "strman strman";
+ * let result = lastIndexOf(title, 'str');
+ * @param {String} value - The String!.
+ * @param {String} needle - Value to search.
+ * @param {Number = undefined} offset - Offset to search.
+ * @param {Boolean = true} caseSensitive - if you use caseSensitive to test.
+ * @return {Number} - Return position of the last occurrence of 'needle'.
  */
 const lastIndexOf = (value, needle, offset = undefined, caseSensitive = true) => {
     if(caseSensitive){
